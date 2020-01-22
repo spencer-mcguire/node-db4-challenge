fe77b64d2b9793ee576a5256a7935dd792c9dfd7
spencer-mcguire <smcguire1826@gmail.com>
2020-01-22 17:30:43 -0600

    initial commit

---
+++ api/recepies/recepie-router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const Recepies = require('./recepie-model');
+
+const router = express.Router();
+
+module.exports = router;
---
+++ api/sever.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const helmet = require('helmet');
+const logger = require('./middleware/customMiddleware');
+
+const recepieRouter = require('../api/recepies/recepie-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(logger);
+server.use(express.json());
+
+server.use('/api/recepies', recepieRouter);
+
+server.get('/', (req, res) => {
+  res.json('API IS Running');
+});
+
+module.exports = server;
