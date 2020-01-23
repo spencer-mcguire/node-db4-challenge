391acf1527aa66ba209cc6dc7e667030a6d01b06
spencer-mcguire <smcguire1826@gmail.com>
2020-01-23 15:29:40 -0600

    get shopping, get instructions

--- api/recipes/recipe-router.js
+++ api/recipes/recipe-router.js
@@ -20,4 +20,30 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id/shoppinglist', (req, res) => {
+  Recipes.getShoppingList(req.params.id)
+    .then(list => {
+      if (list) {
+        res.json(list);
+      } else {
+        res.status(404).json({ error_message: 'No ingredients found.' });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: 'Something happened when getting shopping list' });
+    });
+});
+
+router.get('/:id/instructions', (req, res) => {
+  Recipes.getInstructions(req.params.id)
+    .then(steps => res.json(steps))
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: 'Something happened when getting shopping list' });
+    });
+});
+
 module.exports = router;
--- data/helpers/recipe-model.js
+++ data/helpers/recipe-model.js
@@ -1,7 +1,26 @@
 const db = require('../db-config');
 
-module.exports = { getRecipes };
+module.exports = {
+  getRecipes,
+  getShoppingList,
+  getInstructions
+};
 
 function getRecipes() {
-  return db('recipes');
+  return db('recipes as r').select('r.id', 'r.recipe_name');
+}
+
+function getShoppingList(id) {
+  return db('recipe_ingredient_quantity as a')
+    .select('a.id', 'q.quantity', 'i.ingredient_name')
+    .join('quantities as q', 'a.quantity_id', 'q.id')
+    .join('ingredients as i ', 'a.ingredient_id', 'i.id')
+    .where({ recipe_id: id });
+}
+
+function getInstructions(id) {
+  return db('recipes as r ')
+    .select('r.steps')
+    .where({ id })
+    .then(step => step[0]);
 }
